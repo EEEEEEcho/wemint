bd18615c1645e668c2b53b463d5f0c9b53545100
EEEEEEcho <814466057@qq.com>
2022-11-21 22:50:25 +0800

    开始写兄弟上下文分析部分

--- src/pojo/miniprogram.py
+++ src/pojo/miniprogram.py
@@ -5,9 +5,9 @@ import json
 
 class MiniProgram:
 
-    def __init__(self, base_path: str, name: str):
-        # 文件路径名
-        self.base_path = base_path
+    def __init__(self, path: str, name: str):
+        # 小程序代码所在目录
+        self.path = path
         # 小程序名称
         self.name = name
 
